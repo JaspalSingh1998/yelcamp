d6db08dbe6e5a6e844fe6546c8d71e49761d3744
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-09-18 17:36:20 +0530

    added custom colors and assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Archivo: [],
+      },
+      colors: {
+        "landing-bg": "#F9F6F1",
+        "text-muted": "#595959",
+        "item-border": "#DADADA",
+        accent: "#009FE0",
+      },
+    },
   },
   variants: {
     extend: {},
