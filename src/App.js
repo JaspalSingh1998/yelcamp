3c94f9e0b02fc399eb43a932e37708383dbf83ca
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-09-19 16:24:54 +0530

    Installed React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Navigation from "./components/Navigation";
-// import AllCamps from "./components/AllCamps";
-import SingleCamp from "./components/SingleCamp";
+import AllCamps from "./components/AllCamps";
+// import SingleCamp from "./components/SingleCamp";
 
 function App() {
   return (
     <div className="font-Archivo container mx-auto py-10 px-5">
       <Navigation />
-      <SingleCamp />
+      <AllCamps />
     </div>
   );
 }
---
+++ src/constants/Routes.js
@@ -0,0 +1,5 @@
+export const LANDING = "/";
+export const SIGN_UP = "/signup";
+export const SIGN_IN = "/signin";
+export const ACCOUNT = "/allcamps";
+export const ADMIN = "/singlecamp";
