056bc2e9429d84bdc645eb56a4c94959df3da8df
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-09-19 14:36:11 +0530

    Added Camps Grid

---
+++ src/components/CampsGrid.js
@@ -0,0 +1,118 @@
+import MountUlap from "../Assets/Camp Images/Mount Ulap.jpg";
+import CalaguasIslands from "../Assets/Camp Images/Calaguas Island.jpg";
+import OnayBeach from "../Assets/Camp Images/Onay Beach.jpg";
+import SevenSistersWaterfall from "../Assets/Camp Images/Seven Sisters Waterfall.jpg";
+import LatikRiverside from "../Assets/Camp Images/Latik Riverside.jpg";
+import BuloySprings from "../Assets/Camp Images/Buloy Springs.jpg";
+
+function CampsGrid() {
+  return (
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+      <div className="card border-item-border border rounded-md p-4">
+        <header className="mb-4">
+          <img src={MountUlap} alt="Camp" className="rounded-md mb-4  w-full" />
+          <div>
+            <h3 className="font-bold mb-2">Mount Ulap</h3>
+            <p className="text-text-muted">
+              One of the most famous hikes in Benguet is Mt Ulap in Itogon.
+            </p>
+          </div>
+        </header>
+        <button className="w-full font-bold border border-item-border py-4 rounded-md">
+          View Campground
+        </button>
+      </div>
+      <div className="card border-item-border border rounded-md p-4">
+        <header className="mb-4">
+          <img
+            src={CalaguasIslands}
+            alt="Camp"
+            className="rounded-md mb-4 w-full"
+          />
+          <div>
+            <h3 className="font-bold mb-2">Calaguas Islands</h3>
+            <p className="text-text-muted">
+              A paradise of islands that can rival the white sand beauty of
+              Boracay.
+            </p>
+          </div>
+        </header>
+        <button className="w-full font-bold border border-item-border py-4 rounded-md">
+          View Campground
+        </button>
+      </div>
+      <div className="card border-item-border border rounded-md p-4">
+        <header className="mb-4">
+          <img src={OnayBeach} alt="Camp" className="rounded-md mb-4  w-full" />
+          <div>
+            <h3 className="font-bold mb-2">Onay Beach</h3>
+            <p className="text-text-muted">
+              This is one of the best beach camping sites, beautiful and
+              pristine.
+            </p>
+          </div>
+        </header>
+        <button className="w-full font-bold border border-item-border py-4 rounded-md">
+          View Campground
+        </button>
+      </div>
+      <div className="card border-item-border border rounded-md p-4">
+        <header className="mb-4">
+          <img
+            src={SevenSistersWaterfall}
+            alt="Camp"
+            className="rounded-md mb-4 w-full"
+          />
+          <div>
+            <h3 className="font-bold mb-2">Seven Sisters Waterfall</h3>
+            <p className="text-text-muted">
+              The Seven Sisters is the 39th tallest waterfall in Norway.
+            </p>
+          </div>
+        </header>
+        <button className="w-full font-bold border border-item-border py-4 rounded-md">
+          View Campground
+        </button>
+      </div>
+      <div className="card border-item-border border rounded-md p-4">
+        <header className="mb-4">
+          <img
+            src={LatikRiverside}
+            alt="Camp"
+            className="rounded-md mb-4  w-full"
+          />
+          <div>
+            <h3 className="font-bold mb-2">Latik Riverside</h3>
+            <p className="text-text-muted">
+              Philippines is one of the most dazzling countries in all of Asia.
+            </p>
+          </div>
+        </header>
+        <button className="w-full font-bold border border-item-border py-4 rounded-md">
+          View Campground
+        </button>
+      </div>
+      <div className="card border-item-border border rounded-md p-4">
+        <header className="mb-4">
+          <img
+            src={BuloySprings}
+            alt="Camp"
+            className="rounded-md mb-4  w-full"
+          />
+          <div>
+            <h3 className="font-bold mb-2">Buloy Springs</h3>
+            <p className="text-text-muted">
+              This is one of the best beach camping sites, beautiful and
+              pristine.
+            </p>
+          </div>
+        </header>
+        <button className="w-full font-bold border border-item-border py-4 rounded-md">
+          View Campground
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CampsGrid;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,25 +1,30 @@
+import CampsGrid from "./CampsGrid";
+
 function Search() {
   return (
-    <div className="bg-landing-bg my-10 p-8 ">
-      <h2 className="text-4xl mb-2">Welcome to YelpCamp!</h2>
-      <p className="text-text-muted mb-4 max-w-sm">
-        View our hand-picked campgrounds from all over the world, or add your
-        own.
-      </p>
-      <div className="flex flex-col md:flex-row gap-4 mb-4">
-        <input
-          type="text"
-          placeholder="Search for camp"
-          className="outline-none border border-item-border rounded-md p-4"
-        />
-        <button className="font-bold bg-black text-white p-4 md:px-6 rounded-md">
-          Search
-        </button>
+    <>
+      <div className="bg-landing-bg my-10 p-8 ">
+        <h2 className="text-4xl mb-2">Welcome to YelpCamp!</h2>
+        <p className="text-text-muted mb-4 max-w-sm">
+          View our hand-picked campgrounds from all over the world, or add your
+          own.
+        </p>
+        <div className="flex flex-col md:flex-row gap-4 mb-4">
+          <input
+            type="text"
+            placeholder="Search for camp"
+            className="outline-none border border-item-border rounded-md p-4"
+          />
+          <button className="font-bold bg-black text-white p-4 md:px-6 rounded-md">
+            Search
+          </button>
+        </div>
+        <a href="/" className="text-text-muted underline">
+          or add your own campground
+        </a>
       </div>
-      <a href="/" className="text-text-muted underline">
-        or add your own campground
-      </a>
-    </div>
+      <CampsGrid />
+    </>
   );
 }
 
