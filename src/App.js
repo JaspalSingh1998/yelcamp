696acc091a8c0611b9db6164561ccba88f1a6d2d
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-09-20 10:35:30 +0530

    Refactored Navigation

--- src/App.js
+++ src/App.js
@@ -11,17 +11,17 @@ import * as ROUTES from "./constants/Routes";
 
 function App() {
   return (
-    <div className="font-Archivo container mx-auto py-10 px-5">
+    <div className="font-Archivo">
       <Router>
         <Switch>
           <Route exact path="/" component={Landing} />
           <Route exact path={ROUTES.SIGN_IN} component={SignIn} />
           <Route exact path={ROUTES.SIGN_UP} component={SignUp} />
-          <>
+          <div className="container mx-auto py-10 px-5">
             <Navigation />
             <Route exact path={ROUTES.ALLCAMPS} component={AllCamps} />
             <Route exact path={ROUTES.SINGLECAMP} component={SingleCamp} />
-          </>
+          </div>
         </Switch>
       </Router>
     </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,31 +1,34 @@
 import Logo from "../Assets/Logo.png";
 import Hamburger from "../Assets/Hamburger Menu.svg";
 
+import { Link } from "react-router-dom";
+import * as ROUTES from "../constants/Routes";
+
 function Navigation() {
   return (
     <div className="flex items-center justify-between">
       <div className="flex gap-4 items-center">
         <img src={Logo} alt="Logo" />
-        <a
-          href="/"
+        <Link
+          to={ROUTES.LANDING}
           className="font-bold text-text-muted mt-1 hidden lg:inline-block"
         >
           Home
-        </a>
+        </Link>
       </div>
       <div>
-        <a
-          href="/"
+        <Link
+          to={ROUTES.SIGN_IN}
           className="text-text-muted px-6 py-4 rounded-md hidden lg:inline-block"
         >
           Login
-        </a>
-        <a
-          href="/"
+        </Link>
+        <Link
+          to={ROUTES.SIGN_UP}
           className="bg-black text-white font-bold px-6 py-4 rounded-md hidden lg:inline-block"
         >
           Create an account
-        </a>
+        </Link>
         <img src={Hamburger} alt="Menu Btn" className="lg:hidden" />
       </div>
     </div>
