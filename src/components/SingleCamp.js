bca79ef6eada1350f7bb0eea91935b3ef65f1a70
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-09-23 10:38:13 +0530

    converted camp details into class component

--- src/components/SingleCamp.js
+++ src/components/SingleCamp.js
@@ -1,11 +1,11 @@
 import Map from "./Map";
 import CampDetails from "./CampDetails";
-function SingleCamp() {
+function SingleCamp(props) {
   return (
     <div className="container mx-auto px-5">
       <div className="my-10 grid gap-10 lg:grid-cols-custom">
         <div className="lg:order-2">
-          <CampDetails />
+          <CampDetails id={props.match.params.id} />
         </div>
         <div className="lg:order-1">
           <Map />
